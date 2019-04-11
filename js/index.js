c6366f1907539d6a1dd9a2e50cf08f6e9fbb7633
Jeremy Combe <jeremcombe@hotmail.fr>
2019-04-11 14:28:30 -0400

    delete index.js

--- js/index.js
+++
@@ -1,9 +0,0 @@
-
-function createKeys(){
-
-const Eos = require("eosjs");
-Eos.modules.ecc.randomKey().then(private =>
-  console.log({ private, public: Eos.modules.ecc.privateToPublic(private) }))
-}
-
-createKeys();
