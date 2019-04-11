49f8c69bfcbf293cd851b839407273fd0fe9195f
Jeremy Combe <jeremcombe@hotmail.fr>
2019-04-11 15:09:55 -0400

    add generator name 12 characters 1-5/a-z

--- js/account.js
+++ js/account.js
@@ -5,12 +5,25 @@ let createKeys = function () {
 }
 let createAccount = createKeys();
 
+function randomNameAccount() {
+    var chars = "12345abcdefghiklmnopqrstuvwxyz";
+    var string_length = 12;
+    var randomstring = '';
+    for (var i = 0; i < string_length; i++) {
+        var rnum = Math.floor(Math.random() * chars.length);
+        randomstring += chars.substring(rnum, rnum + 1);
+    }
+    return (randomstring);
+}
+
 createAccount.then(function (result) {
 
+    var nameNewAcct = randomNameAccount();
+
     const config1 = {
-        chainId: "", // 32 byte (64 char) hex string
-        keyProvider: [''], // WIF string or array of keys..
-        httpEndpoint: '', // jungle testnet
+        chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473", // 32 byte (64 char) hex string
+        keyProvider: ['5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'], // WIF string or array of keys..
+        httpEndpoint: 'http://jungle2.cryptolions.io:80', // jungle testnet
         expireInSeconds: 60,
         broadcast: true,
         verbose: false, // API activity
@@ -18,7 +31,7 @@ createAccount.then(function (result) {
     };
 
     options = {
-        authorization: '@active', 
+        authorization: 'aqsxcdfreswx@active',
         broadcast: true,
         sign: true,
     }
@@ -27,23 +40,25 @@ createAccount.then(function (result) {
 
     eos.transaction(tr => {
         tr.newaccount({
-            creator: '',
-            name: '',
+            creator: 'aqsxcdfreswx',
+            name: nameNewAcct,
             owner: result.public,
             active: result.public
         })
         tr.buyrambytes({
-            payer: '',
-            receiver: '',
-            bytes: 2996 
+            payer: 'aqsxcdfreswx',
+            receiver: nameNewAcct,
+            bytes: 2996
         });
-    
+
         tr.delegatebw({
-            from: '',
-            receiver: '',
-            stake_net_quantity: '1.0000 EOS', 
-            stake_cpu_quantity: '1.0000 EOS', 
+            from: 'aqsxcdfreswx',
+            receiver: nameNewAcct,
+            stake_net_quantity: '1.0000 EOS',
+            stake_cpu_quantity: '1.0000 EOS',
             transfer: 0
         });
-    }, options)
+        console.log("NAME: ", nameNewAcct, "PUBLIC KEY:", result.public, "PRIVATE KEY:", result.private);
+    }, options)//.then( (resp) =>{
+       //console.log("=====================================> ", resp)});
 });
