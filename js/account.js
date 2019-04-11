fad569a847b68e658473f1f6adfa3554e1b79712
Jeremy Combe <jeremcombe@hotmail.fr>
2019-04-11 14:39:20 -0400

    add privateKey

--- js/account.js
+++ js/account.js
@@ -1,16 +1,16 @@
 const Eos = require('eosjs');
 
 let createKeys = function () {
-    return Eos.modules.ecc.randomKey().then(private => (({ public: Eos.modules.ecc.privateToPublic(private) })));
+    return Eos.modules.ecc.randomKey().then(private => (({ private, public: Eos.modules.ecc.privateToPublic(private) })));
 }
 let createAccount = createKeys();
 
 createAccount.then(function (result) {
 
     const config1 = {
-        chainId: "", // 32 byte (64 char) hex string
-        keyProvider: [''], // WIF string or array of keys..
-        httpEndpoint: '', // jungle testnet
+        chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473", // 32 byte (64 char) hex string
+        keyProvider: ['5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'], // WIF string or array of keys..
+        httpEndpoint: 'http://jungle2.cryptolions.io:80', // jungle testnet
         expireInSeconds: 60,
         broadcast: true,
         verbose: false, // API activity
@@ -18,30 +18,29 @@ createAccount.then(function (result) {
     };
 
     options = {
-        authorization: '@active', 
+        authorization: 'aqsxcdfreswx@active', 
         broadcast: true,
         sign: true,
     }
     // connects to localhost
     const eos = Eos(config1);
 
-
     eos.transaction(tr => {
         tr.newaccount({
-            creator: '',
-            name: '',
+            creator: 'aqsxcdfreswx',
+            name: 'pockerotoken',
             owner: result.public,
             active: result.public
         })
         tr.buyrambytes({
-            payer: '',
-            receiver: '',
+            payer: 'aqsxcdfreswx',
+            receiver: 'pockerotoken',
             bytes: 2996 
         });
     
         tr.delegatebw({
-            from: '',
-            receiver: '',
+            from: 'aqsxcdfreswx',
+            receiver: 'pockerotoken',
             stake_net_quantity: '1.0000 EOS', 
             stake_cpu_quantity: '1.0000 EOS', 
             transfer: 0
